b221a00df501bedbe7c560d39f2599b65b93d7c2
Alizee <bocianalizee@gmail.com>
2023-08-03 14:26:32 +0200

    routes lobby/:lobbyId et users sont ok

--- routes/lobby.mjs
+++ routes/lobby.mjs
@@ -16,4 +16,30 @@ router.get("/lobby/:lobbyId/users", async (req, res) => {
   }
 })
 
+router.get("/lobby/:lobbyId", async (req, res) => {
+    const lobbyId = req.params.lobbyId
+    try {
+        const result = await pool.query(`SELECT content FROM messages WHERE lobby_id = ${lobbyId}`
+        )
+        res.json(result)
+        console.log(result)
+    } catch (err) {
+        console.error("Error fetching messages", err)
+        res.status(500).json({ error : "Internal Server Error"})
+}
+})
+
+router.get("/lobby/:lobbyId/:messageId", async (req, res) => {
+    const lobbyId = req.params.lobbyId
+    const messageId = req.params.messageId
+    try {
+        const result = await pool.query(`SELECT content FROM messages WHERE lobby_id = ${lobbyId} AND message_id = ${messageId}`
+        )
+        res.json(result)
+    } catch (err) {
+        console.error("Error fetching messages", err)
+        res.status(500).json({ error : "Internal Server Error"})
+}
+})
+
 export default router
--- routes/users.mjs
+++ routes/users.mjs
@@ -2,16 +2,30 @@ import express from "express"
 const router = express()
 import pool from "../server.mjs"
 
-router.get("/users", async (req, res) => {
-  try {
-    const result = await pool.query("select * from users")
+//router.get("/users", async (req, res) => {
+//  try {
+//    const result = await pool.query("select * from users")
 
-    const usernames = result.map((row) => row.username)
-    res.json({ usernames })
+//    const usernames = result.map((row) => row.username)
+//    res.json({ usernames })
+//  } catch (err) {
+//    console.error("Error fetching users:", err)
+//    res.status(500).json({ error: "Internal Server Error" })
+//  }
+//})
+
+
+router.get("/users/:userId", async (req, res) => {
+  const user = req.params.user
+  try {
+    const result = await pool.query(
+     //code ici
+    )
+    res.json(result)
+    console.log(result)
   } catch (err) {
-    console.error("Error fetching users:", err)
+    console.error("Error fetching usernames", err)
     res.status(500).json({ error: "Internal Server Error" })
   }
 })
-
 export default router
